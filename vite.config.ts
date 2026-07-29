25b8e218f843359df96eecf5e11daa12d97a1f69
adubes-admin <adubes@zoller-usa.com>
2026-07-29 09:59:42 -0400

    bug fix 2: Uncaught TypeError: n is not a function

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
+    minify: false,
     cssMinify: false,
   },
   server: {
